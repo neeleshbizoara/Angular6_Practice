c5cc384180869ad82a997d976a240ea1df7a5df1
Neelesh Bizoara <neelesh.bizoara@AD.MSYSTECH.COM>
2018-11-14 18:43:37 +0530

    Implemented Services

--- services-start/src/app/app.component.ts
+++ services-start/src/app/app.component.ts
@@ -1,4 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './service/accounts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
+})
+export class AppComponent implements OnInit {
+  accounts: {name:string, status: string}[] = []
+
+  constructor(private accountService:AccountsService) {}
+
+  ngOnInit() {
+    //Note: since accounts here is an array.
+    // it is a reference type. This is a normal javascript behavior 
+    // so this is a reference type
+
+    //So by setting it equal here we're actully getting access to the 
+    //exact same array as stored in th service
+    this.accounts = this.accountService.accounts;
+  }
+}
+
+
+/* import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -29,3 +55,4 @@ export class AppComponent {
     this.accounts[updateInfo.id].status = updateInfo.newStatus;
   }
 }
+ */
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,35 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../service/logging.service'
+import { AccountsService } from '../service/accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  //@Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService:LoggingService, private accountsService:AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
+    //Commented this aas we are using services 
+
+    /* this.accountAdded.emit({
       name: accountName,
       status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    }); */
+
+    this.accountsService.addAccount(accountName, accountStatus);
+
+    //*******SUPER IMPORTANT************
+    //This is not the way to implement servicesin angular
+    //Correct way is dependency injection 
+    //const service = new LoggingService();
+    //service.logStatusChange(accountStatus);
+
+    // console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
