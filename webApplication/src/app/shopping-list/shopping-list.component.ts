92003e85b78fb1ff20f338c936997a333a0a3049
Neelesh Bizoara <neelesh.bizoara@AD.MSYSTECH.COM>
2018-11-16 19:34:09 +0530

    Adding the Shopping List Service Note: Because Shoppinglist is used in recipe so we need to add ShoppingListService in app.module

--- webApplication/src/app/shopping-list/shopping-list.component.ts
+++ webApplication/src/app/shopping-list/shopping-list.component.ts
@@ -16,8 +16,4 @@ export class ShoppingListComponent implements OnInit {
     this.ingredients = this.shoppingListService.getIngredients();
   }
 
-  OnIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient)
-  }
-
 }
