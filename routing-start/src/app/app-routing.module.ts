3e72549e7fe62c2f66d3dfa8b5215daff852963d
Neelesh Bizoara <neelesh.bizoara@AD.MSYSTECH.COM>
2018-11-19 21:02:42 +0530

    Controlling Navigation with canDeactivate

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 import { HomeComponent } from "./home/home.component";
 import { AuthGuard } from "./auth.guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 
 
 const appRoutes:Routes = [
@@ -22,7 +23,7 @@ const appRoutes:Routes = [
         canActivateChild: [AuthGuard],
         component: ServersComponent, children: [
       { path: ':id', component: ServerComponent},
-      { path: ':id/edit', component: EditServerComponent}
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
     ]},
     {path: 'something', component: PageNotFoundComponent},
     {path: '**', redirectTo: 'something'},//Wild card route 
