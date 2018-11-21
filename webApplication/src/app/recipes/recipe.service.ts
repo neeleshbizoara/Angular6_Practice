7358399e18c2020ddc3e027d4ec2e8643a2aa76b
Neelesh Bizoara <neelesh.bizoara@AD.MSYSTECH.COM>
2018-11-21 21:52:15 +0530

    152. Configuring Route Parameters 153. Passing Dynamic Parameters to Links

--- webApplication/src/app/recipes/recipe.service.ts
+++ webApplication/src/app/recipes/recipe.service.ts
@@ -33,6 +33,10 @@ export class RecipeService {
         return this.recipes.slice();
     }
 
+    getRecipe(index:number) {
+        return this.recipes[index];
+    }
+
     addIngredientsToShoppingList(ingridents: Ingredient[]) {
         this.shoppingListService.addIngredients(ingridents);
     }
