248ab27bd2f31709c49e215b31f1b8539691191c
Neelesh Bizoara <neelesh.bizoara@AD.MSYSTECH.COM>
2018-11-30 00:57:58 +0530

    Building & Using a Custom Observable from Scratch Section 13, Lecture 164

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 import 'rxjs/Rx';
 
 @Component({
@@ -17,6 +17,29 @@ export class HomeComponent implements OnInit {
       (num:number) => {
         console.log(num)
       }
+    )
+    
+    //create method takes function as an argument. and that function holds asynchronous code.
+    const myObservable = Observable.create((observer:Observer<string>) => {
+      setTimeout( () => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout( () => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout( () => {
+        observer.error('Error package');
+        //observer.complete();
+      }, 5000);
+      setTimeout( () => {
+        observer.next('third package');
+      }, 6000);
+    })
+
+    myObservable.subscribe(
+      (data:string) => {console.log(data);},
+      (error:string) => {console.log(error);},
+      () => {console.log('Complete')},
     )
   }
 
